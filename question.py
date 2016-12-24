359dbabcd4370dec87b11a757d562bc2af7ea2c4
hrbeu <jb19900111@163.com>
2016-12-24 18:30:11 +0800

    Get answers of questions on zhihu

--- question.py
+++ question.py
@@ -4,6 +4,7 @@ import json
 from lxml import etree
 from zhuanlan import zhihu
 import operator
+import time
 
 class question(zhihu):
     def __init__(self):
@@ -23,6 +24,7 @@ class question(zhihu):
         count=0
         offset = 0
         while endflag:
+            time.sleep(1)
             params = {"url_token":qid,"pagesize":10,"offset":offset}
             params_json=json.dumps(params)
             data = {
@@ -33,8 +35,11 @@ class question(zhihu):
             self.headers['Referer']=url
             self.headers['Origin']='https://www.zhihu.com'
             self.headers['X-Xsrftoken']=str(qxsrf)
-            r=self.session.post('https://www.zhihu.com/node/QuestionAnswerListV2',data=data,headers=self.headers)
-            content=json.loads(r.content.decode('utf8','ignore'))
+            try:
+                r=self.session.post('https://www.zhihu.com/node/QuestionAnswerListV2',data=data,headers=self.headers)
+                content=json.loads(r.content.decode('utf8','ignore'))
+            except:
+                break
             #print content
             #print content["msg"]
             #print len(content["msg"])
@@ -43,6 +48,18 @@ class question(zhihu):
                 answer = {}
                 answer["html"]=item
                 tree=etree.HTML(item)
+                #提取答主头像的url
+                answer['profile_photo'] = "Anonymous User"      #默认是匿名回答，没有答主头像
+                for node in tree.xpath("//a[@class='zm-item-link-avatar avatar-link']"):        #如果能抓取到头像，则保持头像url地址
+                    answer['profile_photo']=str(node.xpath("img/@src")[0])
+                #提取回答中格式为origin_image zh-lightbox-thumb的图片
+                answer['pics_list'] = []        #默认无图
+                for node in tree.xpath("//img[@class='origin_image zh-lightbox-thumb']"):
+                    answer['pics_list'].append(str(node.xpath("@src")[0]))      #保存全部图片的url
+                #提取回答中格式为content_image的图片
+                answer['images'] = []         #默认无图
+                for node in tree.xpath("//img[@class='content_image']"):
+                    answer['images'].append(str(node.xpath("@src")[0]))     #保存全部图片的url
                 #提取回答内容
                 for node in tree.xpath("//div[@class='zm-editable-content clearfix']"):
                     answer['content']=node.xpath('string(.)')
@@ -61,22 +78,28 @@ class question(zhihu):
                 self.answers.append(answer)
             offset+=10
             count+=len(content["msg"])
+            print u"获取了 %s 个答案" % count
             #如果页面答案数小于10，则到了最后一页，停止爬取
             if int(len(content["msg"])) < 10:
                 endflag=False
+                print
                 print "共捕获%s个回答" % count
     #对答案进行排序
     def answers_sort(self,criterion="editTime"):
         sorted_answers = sorted(self.answers, key=operator.itemgetter(criterion), reverse=True)
         for answer in sorted_answers:
             print answer["content"]
+            print u"作者头像：",answer['profile_photo']
+            #计算回答中两种格式图片的总数
+            pics_count=len(answer['pics_list'])+len(answer['images'])
+            print u"共 %s 张图片" %   pics_count
             print criterion,":",answer[criterion]
         print "\n===========================   THE  END   ====================================\n"
 
 
 a1=question()
 a1.login()
-a1.get_answers('23142571')
+a1.get_answers('29057201')
 a1.answers_sort("commentsCount")
 
 
